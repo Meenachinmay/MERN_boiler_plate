114671ed722aacaa0ce469510d44cb5661bd63e1
Chinmay anand <chinmayanand896@gmail.com>
2020-07-13 04:38:13 +0530

    initial commit, express installed, mongoose installed, mongodb is connected

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// connect the mongodb database
+mongoose.connect('mongodb://localhost:27017/mern-boilerplate', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() => console.log('Database is connected'))
+    .catch((error) => console.log(error));
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+// port to listen
+app.listen(5000);
