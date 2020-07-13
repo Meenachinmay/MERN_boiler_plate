676ef4de866bd29b097c5bbcc36ee28cef0d1952
Chinmay anand <chinmayanand896@gmail.com>
2020-07-13 08:01:54 +0530

    login and register api is working, now login api can generate jwt token

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,6 +39,41 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+//https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// userSchema.methods.generateToken = (callback) => {
+//     var user = this;
+    
+//     var token = jwt.sign({data: user._id},'secret', { expiresIn: 60*60 });
+
+//     user.token = token;
+
+//     user.save((err, user) => {
+//         if (err) return callback(err);
+//         callback(null, user);
+//     });
+// }
 
 const User = mongoose.model('User', userSchema);
 
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const config = require('./config/keys');
 
@@ -22,18 +24,50 @@ app.use(cookieParser());
 
 // New user registration
 app.post('/api/users/register', (req,res) => {
+    // check if email already exists
+    
     const newUser = new User(req.body);
 
-    newUser.save((err, userData) => {
-        if (err) return res.json({ success: false, err});
-        else return res.status(200).json({message: "User is registered successfully!"});
+    newUser.save((err, newlyCreatedUser) => {
+        if (err) return res.status(400).json({ success: false, errors: err});     
+        return res.status(200).json({
+            success: true,
+            userData: newlyCreatedUser
+        });
     });
 
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+// Login user
+app.post('/api/user/login', (req, res) => {
+    // find email
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (err) console.error(err);
+        if (!user){
+            return res.status(400).json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            })
+        }
+
+        // compare password
+        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+            if (err) console.error(err);
+            if (!isMatch){
+                return res.status(400).json({loginSuccess: false, message: "Wrong password"});
+            }
+        });
+
+        // generate token
+        jwt.sign({_id: user._id}, config.JWT_TOKEN_SECRET,(err, token) => {
+            if (err) console.error(err);
+            return res.cookie("x_auth", token).status(200).json({
+                loginSuccess: true,
+                token: token
+            });
+        });
+    });
 });
 
 // port to listen
